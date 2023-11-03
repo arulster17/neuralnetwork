daf2963a54544f4426a846536aa4f5971c2987a5
Arul Mathur <arulster17@gmail.com>
2023-11-03 02:43:01 -0700

    wrote code for the neural network (based on Sebastian Lague's work)

---
+++ DataPoint.java
@@ -0,0 +1,8 @@
+public class DataPoint {
+    double[] inputs;
+    double[] expectedOutputs;
+    public DataPoint(double[] inputs, double[] expectedOutputs) {
+        this.inputs = inputs;
+        this.expectedOutputs = expectedOutputs;
+    }
+}
